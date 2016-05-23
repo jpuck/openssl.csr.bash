226128e0ea1a9c3ef0a235270771cd62a29977ac
Jeff Puckett <jeff@jeffpuckett.com>
2016-05-23 15:38:17 -0500

    add SAN attributes

--- openssl.csr.bash
+++ openssl.csr.bash
@@ -7,4 +7,26 @@ country="US"
 state="AR"
 city="Fayetteville"
 
-openssl req -new -nodes -sha256 -newkey rsa:2048 -keyout $site_name.key -out $site_name.csr -subj "/CN=$site_name/emailAddress=$email_address/O=$organization/OU=$organizational_unit/C=$country/ST=$state/L=$city"
+use_subject_alternative_names=true
+
+declare -a subject_alternative_names=(
+  "www.example.com"
+  "test.example.com"
+  "example.net"
+)
+
+sanstring=""
+for san in ${subject_alternative_names[@]}; do
+  sanstring="$sanstring""DNS:$san,"
+done
+# trim trailing comma
+sanstring=${sanstring::-1}
+
+# get default openssl.cnf
+opensslcnf=$(openssl version -d | cut -d '"' -f2)/openssl.cnf
+
+if $use_subject_alternative_names; then
+  openssl req -new -nodes -sha256 -newkey rsa:2048 -keyout $site_name.key -out $site_name.csr -subj "/CN=$site_name/emailAddress=$email_address/O=$organization/OU=$organizational_unit/C=$country/ST=$state/L=$city" -reqexts SAN -config <(cat $opensslcnf <(printf "[SAN]\nsubjectAltName=$sanstring"))
+else
+  openssl req -new -nodes -sha256 -newkey rsa:2048 -keyout $site_name.key -out $site_name.csr -subj "/CN=$site_name/emailAddress=$email_address/O=$organization/OU=$organizational_unit/C=$country/ST=$state/L=$city"
+fi
